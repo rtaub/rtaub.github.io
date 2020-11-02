c0bec4ee860ece1fcee5a5f3843786137284bca9
rtaub <70655266+rtaub@users.noreply.github.com>
2020-11-02 11:03:15 -0500

    Update clubMeNow.js

--- assignments/clubMeNow.js
+++ assignments/clubMeNow.js
@@ -129,7 +129,7 @@ function resetAllClubDistances() {
 // navigate to "About" screen
 function displayAbout() {
 	// your code here
-	//Creates an alert when the about button is clicked to display the current version of ClubMeNow to the use
+	//Creates an alert when the about button is clicked to display the current version of ClubMeNow to the user
 	alert('ClubMeNow version 1.0.0')
 }
 
